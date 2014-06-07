580aa0e7e331b4a9f013f6d8da37eb6e71a7ef04
emoacht <emotom@pobox.com>
2014-06-07 21:08:34 +0900

    Fixed os version

--- DiskGazer/Helper/OsVersion.cs
+++ DiskGazer/Helper/OsVersion.cs
@@ -8,7 +8,7 @@ namespace DiskGazer.Helper
 {
 	public static class OsVersion
 	{
-		private static readonly OperatingSystem os = Environment.OSVersion;
+		private static readonly Version ver = Environment.OSVersion.Version;
 
 		/// <summary>
 		/// Whether OS is Vista or newer
@@ -16,7 +16,7 @@ namespace DiskGazer.Helper
 		/// <remarks>Windows Vista = version 6.0</remarks>
 		public static bool IsVistaOrNewer
 		{
-			get { return (6 <= os.Version.Major); }
+			get { return (6 <= ver.Major); }
 		}
 
 		/// <summary>
@@ -25,7 +25,7 @@ namespace DiskGazer.Helper
 		/// <remarks>Windows 8 = version 6.2</remarks>
 		public static bool IsEightOrNewer
 		{
-			get { return (6 <= os.Version.Major) && (2 <= os.Version.Minor); }
+			get { return ((6 == ver.Major) && (2 <= ver.Minor)) || (7 <= ver.Major); }
 		}
 	}
 }
