a8035c6fb838123fdab19addc5a9aed02d06d681
emoacht <emotom@pobox.com>
2015-03-30 20:32:30 +0900

    Incremented Revision number

--- DiskGazer/Properties/AssemblyInfo.cs
+++ DiskGazer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.106")]
-[assembly: AssemblyFileVersion("0.5.0.106")]
+[assembly: AssemblyVersion("0.5.0.108")]
+[assembly: AssemblyFileVersion("0.5.0.108")]
